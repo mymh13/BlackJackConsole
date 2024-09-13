17526b84ddeac9b36f6b409aa33e603b27bb93a2
Niklas H <156240330+mymh13@users.noreply.github.com>
2024-09-13 22:06:24 +0200

    initial commit added file

---
+++ BlackJackConsole/BlackJackConsole/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System.ComponentModel.Design;
+
+namespace BlackJackConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Black Jack game!");
+            StartGame();
+        }
+
+        private static void StartGame()
+        {
+            Console.WriteLine("Here you will try to beat the house to 21. Do you know the rules of the game? Y/N");
+            string? rules = Console.ReadLine();
+            if (rules != "y" || "n")
+            {
+                Console.WriteLine("Please enter either Y or N to progress.");
+                StartGame();
+            }
+        }
+    }
+}
